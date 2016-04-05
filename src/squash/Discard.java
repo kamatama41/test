739de59eaed9d975dca0f23013aa6af56eb6b3cd
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-04-06 01:46:44 +0900

    remove discard

--- src/squash/Discard.java
+++
@@ -1,7 +0,0 @@
-package squash;
-
-public class Discard {
-    public static void main(String[] args) {
-        System.out.println("This is discarded in the future!");
-    }
-}
--- src/squash/Foo.java
+++ src/squash/Foo.java
@@ -2,7 +2,6 @@ package squash;
 
 public class Foo {
     public static void main(String[] args) {
-        Discard.main(null);
         System.out.println("Finish!!!!!!!");
     }
 }
